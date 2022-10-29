1feb7db3950fd6df42a439504b24783dba5d1c72
foup <viggonarvaez27@hotmail.com>
2022-10-28 23:14:54 -0400

    Update default.ts

--- apps/bot/config/default.ts
+++ apps/bot/config/default.ts
@@ -4,9 +4,9 @@ export default {
   discord: {
     client: {
       token:
-        "MTAzNDU4NzQxODQ5MTM3NTc5MQ.G9SXXh.jLHDx4chwE4rrfDM_akyFlKb6q4hO6w0XTKPOw",
+        "",
       application: {
-        id: "1034587418491375791",
+        id: "",
       },
       options: {
         intents: [
