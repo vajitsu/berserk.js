2287a6fbc8ae4ab2899c3a3be08ab506b49866f6
foupp <viggonarvaez27@hotmail.com>
2022-10-29 18:29:10 -0400

    update: finish build command

--- packages/riku/src/cli/helpers/build.ts
+++ packages/riku/src/cli/helpers/build.ts
@@ -1,3 +1,79 @@
+import { exec } from "child_process";
+import * as fs from "fs";
+import events from "events";
+import { chalk } from ".";
+
+function getFilesSync(fPath: any, ignore: any, response: string[] = []) {
+  if (!response) {
+    response = [];
+  }
+  if (!ignore) {
+    ignore = [];
+  }
+
+  var files = fs.readdirSync(fPath);
+  for (var i = 0; i < files.length; i++) {
+    if (fs.statSync(fPath + "/" + files[i]).isDirectory()) {
+      var ign = false;
+      for (var j = 0; j < ignore.length; j++) {
+        if (ignore[j] == files[i]) {
+          ign = true;
+          break;
+        }
+      }
+      if (!ign) {
+        response.concat(getFilesSync(fPath + "/" + files[i], ignore, response));
+      }
+    } else {
+      response.push(fPath + "/" + files[i]);
+    }
+  }
+  return response;
+}
+function emptyBuildDir(Events: events.EventEmitter) {
+  const { stdout } = exec("rm -rf .riku/build && mkdir -p .riku/build");
+  stdout?.once("end", () => {
+    console.log();
+    console.log(chalk.msg.riku(chalk.colors.blue(`Emptied build directory.`)));
+    Events.emit("emptied");
+  });
+}
+function tsup(Events: events.EventEmitter) {
+  const { stdout } = exec(
+    `tsup index.ts --outDir .riku/build --clean --minify --format cjs --sourcemap`
+  );
+  stdout?.once("end", () => {
+    console.log();
+    console.log(
+      chalk.msg.riku(chalk.colors.blue(`Compilied files successfully.`))
+    );
+    Events.emit("compilied");
+  });
+}
+function removeTypescriptFromBuildDir(Events: events.EventEmitter) {
+  const { stdout } = exec(
+    "rm -r .riku/build/*.ts && rm -r .riku/build/**/*.ts"
+  );
+  stdout?.once("end", () => {
+    console.log();
+    console.log(
+      chalk.msg.riku(chalk.colors.yellow(`Removed unnecessary typescript.`))
+    );
+    Events.emit("done");
+  });
+}
+
 export default async function build() {
-    void 0;
+  const Events = new events.EventEmitter();
+  Events.once("done", () => {
+    console.log();
+    console.log(
+      chalk.msg.riku(chalk.colors.green("Successfully compilied your project!"))
+    );
+  });
+  Events.once("emptied", () => {
+    tsup(Events);
+  });
+  Events.once("compilied", () => removeTypescriptFromBuildDir(Events));
+  emptyBuildDir(Events);
 }
--- packages/riku/src/cli/index.ts
+++ packages/riku/src/cli/index.ts
@@ -10,22 +10,13 @@ program
   .name("riku")
   .description("Build, test and deploy your Riku.js app")
   .version("0.0.1")
-  .addHelpCommand("help [command]", "Display information for [command]");
+  .addHelpCommand("help [command]", "Display help for [command]");
 
 program
   .command("dev")
   .alias("start")
   .description("Run your Riku.js app")
   .action(() => {
-    // if (options.watch) {
-    //   process.stdout.write(
-    //     `${chalk.msg.riku(chalk.colors.blue(
-    //       "Compiling your code..."
-    //     ))}\n\n`
-    //   );
-    //   compileDev();
-    // }
-
     process.stdout.write(
       `${chalk.msg.riku(chalk.colors.blue("Starting development node..."))}\n\n`
     );
