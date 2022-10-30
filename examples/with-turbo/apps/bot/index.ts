f1039289989d20e9f0904988086abac78159cc60
foupp <viggonarvaez27@hotmail.com>
2022-10-29 22:43:56 -0400

    add: with-turbo example

---
+++ examples/with-turbo/apps/bot/index.ts
@@ -0,0 +1,6 @@
+import * as riku from "riku";
+import config from "./config";
+
+const bot = new riku.bot(config);
+
+export default bot;
---
+++ examples/with-turbo/packages/eslint-config-custom/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: ["next", "turbo", "prettier"],
+  rules: {
+    "@next/next/no-html-link-for-pages": "off",
+    "react/jsx-key": "off",
+    "import/no-anonymous-default-export": "off",
+    "turbo/no-undeclared-env-vars": "off",
+  },
+};
