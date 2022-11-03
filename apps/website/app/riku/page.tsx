84ecd8ce0d0ee1083223e3901b2af1db9ce06efb
foupp <viggonarvaez27@hotmail.com>
2022-11-03 16:41:53 -0400

    update: migrate `next` website to `astro` website

--- apps/website/app/riku/page.tsx
+++
@@ -1,5 +0,0 @@
-import Hero from "@/ui/Riku/Hero";
-
-export default function Riku() {
-  return <Hero />;
-}
---
+++ apps/website/astro.config.mjs
@@ -0,0 +1,28 @@
+import { defineConfig } from "astro/config";
+
+import mdx from "@astrojs/mdx";
+import prefetch from "@astrojs/prefetch";
+import react from "@astrojs/react";
+import vercel from "@astrojs/vercel/edge";
+
+import theme from "shiki/themes/dark-plus.json" assert { type: "json" };
+import { remarkCodeHike } from "@code-hike/mdx";
+
+// https://astro.build/config
+export default defineConfig({
+  output: "server",
+  markdown: {
+    remarkPlugins: [
+      [remarkCodeHike, { autoImport: false, theme, lineNumbers: true }],
+    ],
+    extendDefaultPlugins: true,
+  },
+  integrations: [
+    mdx(),
+    react(),
+    vercel(),
+    prefetch({
+      throttle: 3,
+    }),
+  ],
+});
