cc0ba694328e21a6fa66325499f0656cded218ae
foupp <viggonarvaez27@hotmail.com>
2022-11-03 22:43:35 -0400

    update: switch from the `edge` to serverless

--- apps/website/astro.config.mjs
+++ apps/website/astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import prefetch from "@astrojs/prefetch";
 import react from "@astrojs/react";
-import vercel from "@astrojs/vercel/edge";
+import vercel from "@astrojs/vercel/serverless";
 
 import theme from "shiki/themes/slack-dark.json" assert { type: "json" };
 import { remarkCodeHike } from "@code-hike/mdx";
