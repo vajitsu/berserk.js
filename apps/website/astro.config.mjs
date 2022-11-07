b8e3188bd3a1a899b022123f7810d9f1ac7296e1
foupp <93486795+kiratsu@users.noreply.github.com>
2022-11-07 00:59:53 -0500

    update: whole new setup for documentation

--- apps/website/astro.config.mjs
+++ apps/website/astro.config.mjs
@@ -5,26 +5,12 @@ import prefetch from "@astrojs/prefetch";
 import react from "@astrojs/react";
 import vercel from "@astrojs/vercel/serverless";
 
-import theme from "shiki/themes/slack-dark.json" assert { type: "json" };
-import { remarkCodeHike } from "@code-hike/mdx";
-
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   markdown: {
-    remarkPlugins: [
-      [
-        remarkCodeHike,
-        {
-          autoImport: false,
-          theme,
-          lineNumbers: true,
-          showCopyButton: false,
-          staticMediaQuery: "(max-width: 951px)",
-        },
-      ],
-    ],
     extendDefaultPlugins: true,
+    syntaxHighlight: "prism",
   },
   integrations: [
     mdx(),
