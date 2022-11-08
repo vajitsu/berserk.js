3e9ed3e1729dcae881bfa5002f350362a7a44739
foupp <93486795+kiratsu@users.noreply.github.com>
2022-11-07 21:16:00 -0500

    update: another attempt to fix bug

--- apps/website/astro.config.mjs
+++ apps/website/astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import prefetch from "@astrojs/prefetch";
 import react from "@astrojs/react";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/static";
 
 import rehypeRewrite from "rehype-rewrite";
 
