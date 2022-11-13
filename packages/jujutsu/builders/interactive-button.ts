418fec97603ab685054055ec91d1b9fdade1dda3
kiratsu <93486795+kiratsu@users.noreply.github.com>
2022-11-13 11:52:13 -0500

    add: interactive button builder

---
+++ packages/jujutsu/builders/interactive-button.ts
@@ -0,0 +1,61 @@
+import Discord from "discord.js";
+
+export type URLButton = {
+  label: string;
+  style: "link";
+  disabled?: boolean;
+  emoji?: Discord.ComponentEmojiResolvable;
+  url: string;
+};
+
+export type ButtonData = {
+  customId: string;
+  label: string;
+  style: "primary" | "secondary" | "success" | "danger";
+  disabled?: boolean;
+  emoji?: Discord.ComponentEmojiResolvable;
+};
+
+export function ButtonData(
+  data: ButtonData
+): Partial<Discord.APIButtonComponentWithCustomId> {
+  const button = new Discord.ButtonBuilder();
+
+  switch (data.style) {
+    case "primary":
+      button.setStyle(1);
+      break;
+    case "secondary":
+      button.setStyle(2);
+      break;
+    case "success":
+      button.setStyle(3);
+      break;
+    case "danger":
+      button.setStyle(4);
+      break;
+    // case "link":
+    //   button.setStyle(5);
+    //   break;
+  }
+
+  button.setLabel(data.label);
+
+  if (data.emoji) button.setEmoji(data.emoji);
+  if (typeof data.disabled === "boolean") button.setDisabled(data.disabled);
+
+  //if (data.style === "link") button.setURL(data.url);
+  /*else*/
+  button.setCustomId(data.customId);
+
+  return button.data as Partial<Discord.APIButtonComponentWithCustomId>;
+}
+
+export default abstract class InteractiveButton {
+  abstract data: Partial<Discord.APIButtonComponentWithCustomId>;
+
+  abstract run(
+    interaction: Discord.ButtonInteraction,
+    client: Discord.Client
+  ): Promise<void>;
+}
