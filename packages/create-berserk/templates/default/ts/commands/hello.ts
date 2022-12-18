547f3a97efda22f29e4c86a8c22996f3028468b6
foupp <viggonarvaez27@hotmail.com>
2022-12-18 14:38:12 -0500

    Update linting

--- packages/create-berserk/templates/default/ts/commands/hello.ts
+++ packages/create-berserk/templates/default/ts/commands/hello.ts
@@ -1,9 +1,7 @@
 export const description = 'Say hello!'
 
-import { JujutsuInteraction, JujutsuCommand } from 'berserk'
+import { DiscordJsInteraction } from 'berserk'
 
-export default async function Hello(
-  interaction: JujutsuInteraction
-): JujutsuCommand {
+export default async function Hello(interaction: DiscordJsInteraction) {
   interaction.reply(`Hello, ${interaction.user?.tag}`)
 }
