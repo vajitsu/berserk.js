b9eac3088471d0f93c6bde95f1869c8d968c89a3
kiratsu <93486795+kiratsu@users.noreply.github.com>
2022-12-25 17:42:27 -0500

    Remove packages that conflict to currency 'canary' branch

--- packages/create-berserk/templates/app/js/app/hello/command.js
+++
@@ -1,7 +0,0 @@
-// ESM supported out of the box without having enabling it (dont worry, CommonJS is still supported)
-
-export const description = 'Say hello!'
-
-export default function Hello(interaction) {
-  interaction.reply(`Hello, ${interaction.user?.tag}`)
-}
