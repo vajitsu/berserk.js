258cce95f7066dedb47d99750490ded989f351d3
kijv <93486795+kijv@users.noreply.github.com>
2022-12-25 23:27:11 +0000

    Update lock file

--- packages/create-jujutsu/templates/app/ts/app/hello/command.ts
+++
@@ -1,7 +0,0 @@
-export const description = 'Say hello!'
-
-import { DiscordJsInteraction } from 'jujutsu'
-
-export default async function Hello(interaction: DiscordJsInteraction) {
-  interaction.reply(`Hello, ${interaction.user?.tag}`)
-}
